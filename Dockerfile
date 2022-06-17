98d740e450d5e8ae888261842cdaaaa7f6775659
Amartya Chakraborty <amartyachakraborty@Amartyas-MBP-2.fios-router.home>
2022-06-17 16:43:27 -0400

    Add support for protobuf to DRAIN service

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ WORKDIR /code
 COPY ./drain-service/drain_training_inferencing.py .
 COPY ./drain-service/drain_pretrained_inferencing.py .
 COPY ./drain-service/drain_modules.py .
+COPY ./drain-service/payload_pb2.py .
 COPY ./drain-service/drain3.ini .
 COPY ./drain-service/drain3_control_plane_model_v0.4.1.bin .
 ADD ./drain-service/drain3 /code/drain3
+RUN pip install protobuf==3.19.4
 
 CMD ["python", "./drain_modules.py"]
--- drain-service/drain_pretrained_inferencing.py
+++ drain-service/drain_pretrained_inferencing.py
@@ -3,13 +3,14 @@ import asyncio
 from asyncio.exceptions import TimeoutError
 import json
 import logging
-import os
 import sys
 import time
 from io import StringIO
 
 # Third Party
+from google.protobuf import json_format
 import pandas as pd
+import payload_pb2
 from drain3.template_miner import TemplateMiner
 from opni_nats import NatsWrapper
 
@@ -19,6 +20,11 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(messa
 cp_template_miner = TemplateMiner()
 
 nw = NatsWrapper()
+
+def get_serialized_protobuf_object(logs_dict_list):
+    payload_list = payload_pb2.PayloadList()
+    protobuf_logs = {"items": logs_dict_list}
+    return (json_format.ParseDict(protobuf_logs, payload_list)).SerializeToString()
 
 async def load_pretrain_model():
     # This function will load the pretrained DRAIN model for control plane logs in addition to the anomaly level for each template.
@@ -30,13 +36,13 @@ async def load_pretrain_model():
         logging.error(f"Unable to load DRAIN model {e}")
         return False
 
-async def consume_logs(incoming_cp_logs_queue, logs_to_update_es_cp):
+async def consume_logs(incoming_cp_logs_queue):
     # This function will subscribe to the Nats subjects preprocessed_logs_control_plane and anomalies.
     async def subscribe_handler(msg):
-        payload_data = msg.data.decode()
-        await incoming_cp_logs_queue.put(
-            pd.read_json(StringIO(payload_data), dtype={"_id": object, "cluster_id": str, "ingest_at": str})
-        )
+        payload_data = msg.data
+        log_payload_list = payload_pb2.PayloadList()
+        logs_df = pd.DataFrame(json_format.MessageToDict(log_payload_list.FromString(payload_data))["items"])
+        await incoming_cp_logs_queue.put(logs_df)
 
     await nw.subscribe(
         nats_subject="preprocessed_logs_pretrained_model",
@@ -59,33 +65,29 @@ async def inference_logs(incoming_logs_queue):
         cp_model_logs = []
         rancher_model_logs = []
         for index, row in logs_df.iterrows():
-            log_message = row["masked_log"]
+            log_message = row["maskedLog"]
             if log_message:
                 row_dict = row.to_dict()
                 template = cp_template_miner.match(log_message)
                 if template:
-                    row_dict["anomaly_level"] = template.get_anomaly_level()
-                    row_dict["drain_pretrained_template_matched"] = template.get_template()
-                    row_dict["inference_model"] = "drain"
+                    row_dict["anomalyLevel"] = template.get_anomaly_level()
+                    row_dict["drainPretrainedTemplateMatched"] = template.get_template()
+                    row_dict["inferenceModel"] = "drain"
                     logs_inferenced_results.append(row_dict)
                 else:
-                    if row["log_type"] == "controlplane":
+                    if row["logType"] == "controlplane":
                         cp_model_logs.append(row_dict)
-                    elif row["log_type"] == "rancher":
+                    elif row["logType"] == "rancher":
                         rancher_model_logs.append(row_dict)
-        start_time = time.time()
         if (start_time - last_time >= 1 and len(logs_inferenced_results) > 0) or len(logs_inferenced_results) >= 128:
-            logs_inferenced_drain_df = (pd.DataFrame(logs_inferenced_results).to_json().encode())
-            await nw.publish("inferenced_logs", logs_inferenced_drain_df)
+            await nw.publish("inferenced_logs", get_serialized_protobuf_object(logs_inferenced_results))
             logs_inferenced_results = []
             last_time = start_time
         if len(cp_model_logs) > 0:
-            model_logs_json = json.dumps(cp_model_logs).encode()
-            await nw.publish("opnilog_cp_logs", model_logs_json)
+            await nw.publish("opnilog_cp_logs", get_serialized_protobuf_object(cp_model_logs))
             logging.info(f"Published {len(cp_model_logs)} logs to be inferenced on by Control Plane Deep Learning model.")
         if len(rancher_model_logs) > 0:
-            rancher_logs_json = json.dumps(rancher_model_logs).encode()
-            await nw.publish("opnilog_rancher_logs", rancher_logs_json)
+            await nw.publish("opnilog_rancher_logs", get_serialized_protobuf_object(rancher_model_logs))
             logging.info(f"Published {len(rancher_model_logs)} logs to be inferenced on by Rancher Deep Learning model.")
         logging.info(f"{len(logs_df)} logs processed in {(time.time() - start_time)} second(s)")
 
@@ -101,7 +103,6 @@ async def init_nats():
 def main():
     loop = asyncio.get_event_loop()
     incoming_cp_logs_queue = asyncio.Queue(loop=loop)
-    cp_logs_to_update_in_elasticsearch = asyncio.Queue(loop=loop)
     init_nats_task = loop.create_task(init_nats())
     loop.run_until_complete(init_nats_task)
 
@@ -110,9 +111,7 @@ def main():
     if not model_loaded:
         sys.exit(1)
 
-    preprocessed_logs_consumer_coroutine = consume_logs(
-        incoming_cp_logs_queue, cp_logs_to_update_in_elasticsearch
-    )
+    preprocessed_logs_consumer_coroutine = consume_logs(incoming_cp_logs_queue)
 
     match_cp_logs_coroutine = inference_logs(incoming_cp_logs_queue)
 
