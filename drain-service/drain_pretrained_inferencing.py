dd8f1710ede0a761f899998eaf97975b4cbbedf4
Amartya Chakraborty <chakrab.amartya@gmail.com>
2022-08-04 13:45:20 -0700

    Address PR comments to not make pretrained template_miner object a global variable

--- drain-service/drain_pretrained_inferencing.py
+++ drain-service/drain_pretrained_inferencing.py
@@ -11,7 +11,6 @@ from drain3.template_miner import TemplateMiner
 from opni_nats import NatsWrapper
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(message)s")
-pretrained_template_miner = TemplateMiner()
 
 nw = NatsWrapper()
 
@@ -29,15 +28,16 @@ def match_template(log_data, template_miner):
 async def load_pretrain_model():
     # This function will load the pretrained DRAIN model for control plane logs in addition to the anomaly level for each template.
     try:
+        pretrained_template_miner = TemplateMiner()
         pretrained_template_miner.load_state("drain3_control_plane_model_v0.6.0.bin")
         num_pretrained_clusters = pretrained_template_miner.drain.clusters_counter
         logging.info("Able to load the DRAIN control plane model with {} clusters.".format(num_pretrained_clusters))
         persistence = FilePersistence("drain3_non_workload_model.bin")
         current_template_miner = TemplateMiner(persistence_handler=persistence, clusters_counter=num_pretrained_clusters)
-        return True, current_template_miner
+        return pretrained_template_miner, current_template_miner
     except Exception as e:
         logging.error(f"Unable to load DRAIN model {e}")
-        return False, None
+        return None, None
 
 async def consume_logs(incoming_cp_logs_queue, update_model_logs_queue, batch_processed_queue):
     # This function will subscribe to the Nats subjects preprocessed_logs_control_plane and anomalies.
@@ -83,7 +83,7 @@ async def persist_model(batch_processed_queue, current_template_miner):
             last_upload = time.time()
 
 
-async def inference_logs(incoming_logs_queue, current_template_miner):
+async def inference_logs(incoming_logs_queue, pretrained_template_miner, current_template_miner):
     '''
         This function will be inferencing on logs which are sent over through Nats and using the DRAIN model to match the logs to a template.
         If no match is made, the log is then sent over to be inferenced on by the Deep Learning model.
@@ -157,13 +157,13 @@ def main():
     init_nats_task = loop.create_task(init_nats())
     loop.run_until_complete(init_nats_task)
     init_model_task = loop.create_task(load_pretrain_model())
-    model_loaded, current_template_miner = loop.run_until_complete(init_model_task)
-    if not model_loaded:
+    pretrained_template_miner, current_template_miner = loop.run_until_complete(init_model_task)
+    if not pretrained_template_miner:
         sys.exit(1)
 
     preprocessed_logs_consumer_coroutine = consume_logs(incoming_cp_logs_queue, update_model_logs_queue, batch_processed_queue)
 
-    match_cp_logs_coroutine = inference_logs(incoming_cp_logs_queue, current_template_miner)
+    match_cp_logs_coroutine = inference_logs(incoming_cp_logs_queue, pretrained_template_miner, current_template_miner)
 
     update_model_coroutine = update_model(update_model_logs_queue, current_template_miner)
 
